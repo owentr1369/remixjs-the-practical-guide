0921d68e9f5288835ac29b64ac706c64cb7e6f08
Tâm Trần Thiện <tamvaa1306@gmail.com>
2025-03-05 13:39:33 +0700

    add components for logo, error handling, modal, expenses list, and statistics; update auth page with AuthForm

--- section-3/app/routes/auth.tsx
+++ section-3/app/routes/auth.tsx
@@ -2,13 +2,17 @@ import React from "react";
 
 import authStyles from "~/styles/auth.css";
 import { LinksFunction } from "@remix-run/node";
-
+import AuthForm from "~/components/auth/AuthForm";
 export const link: LinksFunction = () => [
   { rel: "stylesheet", href: authStyles },
 ];
 
 const AuthPage = () => {
-  return <div>AuthPage</div>;
+  return (
+    <div>
+      <AuthForm />
+    </div>
+  );
 };
 
 export default AuthPage;
--- section-3/app/routes/expenses.analysis.tsx
+++ section-3/app/routes/expenses.analysis.tsx
@@ -1,7 +1,42 @@
 import React from "react";
+import ExpenseStatistics from "~/components/expenses/ExpenseStatistics";
+import Chart from "~/components/expenses/Chart";
+
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  {
+    id: "e2",
+    title: "New TV",
+    amount: 799.49,
+    date: new Date(2021, 2, 12),
+  },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
 
 const ExpensesAnalysisPage = () => {
-  return <div>ExpensesAnalysisPage</div>;
+  return (
+    <div>
+      <h1 className="text-center">Expenses Analysis</h1>
+      <Chart expenses={DUMMY_EXPENSES} />
+      <ExpenseStatistics expenses={DUMMY_EXPENSES} />
+    </div>
+  );
 };
 
 export default ExpensesAnalysisPage;
