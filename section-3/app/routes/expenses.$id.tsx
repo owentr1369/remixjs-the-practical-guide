ebd50c5c03fe8e7a433adde430887c7acf239d87
Tâm Trần Thiện <tamvaa1306@gmail.com>
2025-03-05 13:44:46 +0700

    add ExpenseForm to ExpensesDynamicPage for expense updates

--- section-3/app/routes/expenses.$id.tsx
+++ section-3/app/routes/expenses.$id.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import ExpenseForm from "~/components/expenses/ExpenseForm";
 
 const ExpensesDynamicPage = () => {
-  return <div>ExpensesDynamicPage</div>;
+  return (
+    <div>
+      <h1 className="text-center">Update Expense</h1>
+      <ExpenseForm />
+    </div>
+  );
 };
 
 export default ExpensesDynamicPage;
