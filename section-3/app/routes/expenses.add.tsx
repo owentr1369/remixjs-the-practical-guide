6799a599cdb5d8e69c365aa8cfae3e6a3f326479
Tâm Trần Thiện <tamvaa1306@gmail.com>
2025-03-05 13:41:34 +0700

    add ExpenseForm component to ExpensesAddPage for expense addition

--- section-3/app/routes/expenses.add.tsx
+++ section-3/app/routes/expenses.add.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import ExpenseForm from "~/components/expenses/ExpenseForm";
 
 const ExpensesAddPage = () => {
-  return <div>ExpensesAddPage</div>;
+  return (
+    <div>
+      <h1 className="text-center">Add Expense</h1>
+      <ExpenseForm />
+    </div>
+  );
 };
 
 export default ExpensesAddPage;
